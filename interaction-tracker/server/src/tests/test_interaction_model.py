83d6e40c45805944b5da476f8f8c66bbba0d718a
Declan Kene Chukwu <declan.chukwu@ucdconnect.ie>
2025-12-15 21:00:49 +0000

    working tests which pass for create interactions api

--- interaction-tracker/server/src/tests/test_interaction_model.py
+++ interaction-tracker/server/src/tests/test_interaction_model.py
@@ -27,36 +27,32 @@ def test_create_interaction_return_201_and_object():  # pytest function
     assert isinstance(data, dict) 
 
 
-# def test_create_interaction_detects_badly_formatted_data():
-#     response = client.post("/api/interactions", 
-#         json={
-#             "event_type": "page_view",
-#             "metadata": {
-#                 "page": "/dashboard",
-#                 "referrer": "https://google.com",
-#                 "device": "desktop",
-#                 "browser": "Chrome"
-#             }
-#         }  
-#     )  # Use TestClient
+def test_create_interaction_detects_badly_formatted_data():
+    with TestClient(app=app) as client:
+        response = client.post("/api/interactions", 
+            json={
+                "event_type": "page_view",
+                "metadata": {
+                    "page": "/dashboard",
+                    "referrer": "https://google.com",
+                    "device": "desktop",
+                    "browser": "Chrome"
+                }
+            }  
+        )  # Use TestClient
 
-#     # should raise:
-#     # raise HTTPException(
-#     # status_code=422,
-#     # detail="Missing required field: user_id"
-#     assert response.status_code == 422
+    # should raise:
+    # raise HTTPException(
+    # status_code=422,
+    # detail="Missing required field: user_id"
+    assert response.status_code == 422
 
-# def test_create_interaction_detects_invalid_json_format():
-
-#     response = client.post("/api/interactions", 
-#         json={ "user_id": "user_123", "event_type": 123, "metadata": "not-an-object" }
-#     )  # Use TestClient
-
-#     # should raise:
-#     # raise HTTPException(
-#     # status_code=422,
-#     # detail="Metadata not an object"
-#     assert response.status_code == 422
+def test_create_interaction_detects_invalid_type():
+    with TestClient(app=app) as client:
+        response = client.post("/api/interactions", 
+            json={ "user_id": "user_123", "event_type": 123, "metadata": "not-an-object" }
+        )  # Use TestClient
+    assert response.status_code == 422
 
 
 # # def test_missing_user_id_raises_validation_error()
